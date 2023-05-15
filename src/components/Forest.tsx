f75a446008b7d8e5f43b1e422f26ed1bc3ff42fc
mpakunderscore <mpakunderscore@gmail.com>
2023-05-15 21:40:12 +0400

    Update canvas

--- src/components/Forest.tsx
+++ src/components/Forest.tsx
@@ -10,7 +10,7 @@ import Canvas from "../canvas/Canvas";
 const Forest = (props) => {
 
     const {selectedItem, setSelectedItem} = useContext(UserContext)
-    const {centerView} = useContext(MapContext)
+    const {currentPositionX, currentPositionY, centerView, cellSize} = useContext(MapContext)
 
     const clickTile = (x, y) => {
 
@@ -24,11 +24,30 @@ const Forest = (props) => {
         ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
 
 
-        ctx.beginPath()
-        ctx.moveTo(300, 300)
-        ctx.lineTo(700, 700)
-        ctx.strokeStyle = '#ffffff'
-        ctx.stroke()
+        if (props.user.userItems) {
+            for (let i = 0; i < props.user.userItems.length; i++) {
+
+                let item = props.user.userItems[i]
+                const nextItem = props.user.userItems[i + 1]
+
+                if (nextItem) {
+
+                    const startLineX = (item.x - currentPositionX) * cellSize + cellSize/1.3
+                    const startLineY = (item.y - currentPositionY) * cellSize + cellSize/1.3
+
+                    const endLineX = (nextItem.x - currentPositionX) * cellSize + cellSize/1.3
+                    const endLineY = (nextItem.y - currentPositionY) * cellSize + cellSize/1.3
+
+                    ctx.beginPath()
+                    ctx.moveTo(startLineX, startLineY)
+                    ctx.lineTo(endLineX, endLineY)
+                    ctx.strokeStyle = '#0d1810'
+                    ctx.stroke()
+                }
+            }
+        }
+
+
 
         // ctx.beginPath()
         // ctx.arc(50, 100, 20*Math.sin(frameCount*0.05)**2, 0, 2*Math.PI)
