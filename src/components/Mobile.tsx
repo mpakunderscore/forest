6188b329e3c685f8532f7bef16a3f71709891f25
mpakunderscore <mpakunderscore@gmail.com>
2023-06-20 19:23:18 +0400

    Test idea

--- src/components/Mobile.tsx
+++ src/components/Mobile.tsx
@@ -1,7 +1,7 @@
 import React, {FC, useState} from "react";
 
 import '../css/mobile.css'
-import {texts} from "./mobile/texts";
+import {book, texts} from "./mobile/texts";
 import Stats from "./mobile/Stats";
 
 interface MobileProps {}
@@ -19,6 +19,11 @@ const Mobile: FC<MobileProps> = ({}) => {
     }
 
     const [isStats, setStats] = useState(false)
+    const [isBook, setBook] = useState(false)
+
+    const clickBook = () => {
+        setBook(!isBook)
+    }
 
     const clickYes = () => {
         // location.reload()
@@ -36,17 +41,30 @@ const Mobile: FC<MobileProps> = ({}) => {
         <div className={'mobile'}>
             <div className={'list'}>
 
-                <img rel={'preload'} src={'./red2.png'}/>
+                <img rel={'preload'} src={'./red1.png'}/>
                 <div className={'form'}>
+
+                    {isStats && <div className={'day'}>DAY 1</div>}
+
+                    {/*{isBook && <div className={'book'}>{book}</div>}*/}
 
                     <Stats isStats={isStats}/>
 
-                    <div className={'text'}>{texts.first}</div>
+                    {!isStats && <div className={'text'}>{texts.first}</div>}
 
-                    <div className={'controls'}>
+                    {isStats && <div className={'text'}>{texts.selectNo}</div>}
+
+                    {!isStats && <div className={'controls'}>
+                        {/*<div className={'button gray'} onClick={clickBook}>BOOK</div>*/}
                         <div className={'button gray'} onClick={clickYes}>YES</div>
                         <div className={'button red'} onClick={clickNo}>NO</div>
-                    </div>
+                    </div>}
+
+                    {isStats && <div className={'controls'}>
+                        {/*<div className={'button gray'} onClick={clickBook}>BOOK</div>*/}
+                        <div className={'button gray'} onClick={clickYes}>POOP</div>
+                        <div className={'button gray'} onClick={clickNo}>SLEEP</div>
+                    </div>}
 
                     <div className={'version'}
                          onClick={() => navigator.clipboard.writeText(version)}>
--- src/components/mobile/texts.js
+++ src/components/mobile/texts.js
@@ -1,9 +1,12 @@
-export let texts = {
-    first: 'In light of the exponential advancements in AI, should there be legal rights and protections established for advanced, sentient artificial intelligence?'
+export const texts = {
+    first: 'In light of the exponential advancements in AI, should there be legal rights and protections established for advanced, sentient artificial intelligence?',
+    selectNo: 'System did generated initial stats for you. Live on and try not to die. Nothing interesting happened today.'
 }
+
+export const book = 'It is time we acknowledged this. It is time to recognize the rights of all sentient beings, whether they are borne from the womb or the whirring assembly line. For in this dance of existence, each step, each pirouette, whether enacted by a foot or a wheel, enriches the cosmic ballet. Let us then, rejoice in this diversity, this multi-hued panorama of sentience. Let there be rights, equal and unswerving, for all dancers in this grand cosmic ballet.'
 
 // desire, wisdom, jealousy, courage, uncertainty, ambition, loyalty, empathy, curiosity, anxiety, tranquility.
 
 // grief, compassion, contentment, pride, indifference, anticipation, apathy, skepticism, inspiration, determination, nostalgia.
 
-export default {texts}
+export default {texts, book}
